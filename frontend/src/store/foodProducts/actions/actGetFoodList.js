7bd3b9f07d16fcaa62644948550bd13aa8afddd4
M.Plazza <106887594+MoohamedPK@users.noreply.github.com>
2025-02-04 22:22:26 +0100

    Update actGetFoodList.js

--- frontend/src/store/foodProducts/actions/actGetFoodList.js
+++ frontend/src/store/foodProducts/actions/actGetFoodList.js
@@ -5,7 +5,7 @@ const actGetFoodList = createAsyncThunk("food/actGetFoodList", async(_ ,{rejectW
 
     try {
         
-        const response = await axios.get("http://localhost:5000/api/food/list");
+        const response = await axios.get("https://food-delivery-web-backend-xjvc.onrender.com/api/food/list");
         return response.data.data
 
     } catch (error) {
